2b82261fe28588db119db722f0704deaec715688
lirunfang <cctaev@163.com>
2024-01-13 09:49:02 +0800

    feat: SpringBoot自动装配原理

---
+++ auto-config-principle/src/main/java/com/example/autoconfigprinciple/AutoConfigPrincipleApplication.java
@@ -0,0 +1,13 @@
+package com.example.autoconfigprinciple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoConfigPrincipleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoConfigPrincipleApplication.class, args);
+	}
+
+}
---
+++ auto-config-principle/src/test/java/com/example/autoconfigprinciple/AutoConfigPrincipleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autoconfigprinciple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoConfigPrincipleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
